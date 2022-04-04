d35c9d05033acce78527d0a1353cb97648e9a229
Alexey <alexeyBarsAlexey@mail.ru>
2022-04-04 22:25:39 +0300

    added favicon

---
+++ best-app/src/components/Header/InputSearch/InputSearch.jsx
@@ -0,0 +1,12 @@
+// import { SearchInput } from "./InputSearch.styles"
+
+
+// export const InputSearch = ({ placeholder = 'Поиск...' }) => {
+
+//   return (
+//     <>
+//       <SearchInput type="text" placeholder={placeholder} />
+
+//     </>
+//   )
+// };
